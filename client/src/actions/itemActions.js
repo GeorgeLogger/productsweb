3e17eab9914dd791a7c4bbeeadece1345620f920
GeorgeLogger <georgeisloggedin@gmail.com>
2021-09-15 20:28:48 +0900

    Successfully implemented Redux to Hooks

---
+++ client/src/actions/itemActions.js
@@ -0,0 +1,11 @@
+import { GET_ITEMS, ADD_ITEM } from "./types";
+
+export const getItems = () => (dispatch) => {
+  const action = { type: GET_ITEMS };
+  dispatch(action);
+};
+
+export const addItem = (newItem) => (dispatch) => {
+  const action = { type: ADD_ITEM, payload: { newItem } };
+  dispatch(action);
+};
